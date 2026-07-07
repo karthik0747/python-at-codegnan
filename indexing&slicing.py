f73b301735ce0b3cc669250a5a53fd0231ba600a
karthik0747 <karthikustelamuri@gmail.com>
2026-07-07 22:25:42 +0530

    Add files via upload

---
+++ indexing&slicing.py
@@ -0,0 +1,23 @@
+print("Strings Intro")
+#accessing the string char by using indexing
+s="codegnan"
+'''print(s[0])
+print(s[1])
+print(s[2])
+print(s[3])
+print(s[4])
+print(s[5])
+
+#Slicing
+
+print(s[0:8:1])
+print(s[0:8:2])
+print(s[-1:-5:-1])
+print(s[-2:-5])
+print(s[4:-1])
+print(s[::-1])'''
+
+s1=s[:4]
+s2='z'
+s3=s[5:]
+print(s1+s2+s3)
---
+++ operators.py
@@ -0,0 +1,79 @@
+#operators
+print("ARITHMETIC OPERATORS ")
+a=10
+b=20
+print("addition:",a+b)
+print("subtraction:",a-b)
+print("multiplication:",a*b)
+print("division:",a/b)
+print("floor division:",a//b)
+print("modulo division:",a%b)
+
+
+print("ASSIGNMENT OPERATORS")
+a=5
+a+=1
+print("assig addition:",a)
+a=7
+print("assig subtraction:",a)
+a=7
+a/=2
+print("assig division:",a)
+a=5
+a*=2
+print("assig multiplication:",a)
+a=5
+a//=2
+print("assig floor:",a)
+a=5
+a%=2
+print("assig modulo:",a)
+
+print("RELATIONAL OPERATORS")
+a=5
+b=7
+print("less than:",a<b)
+print("greater than:",a>b)
+print("greater or equal:",a>=b)
+print("less or equal:",a<=b)
+print("equal to:",a==b)
+print("not equal:",a!=b)
+
+print("LOGICAL OPERATORS")
+a=7
+b=5
+print("and operator:",a>b and a<b)
+print("or operator:",a>b or a<b)
+
+
+print("BITWISE OPERATORS")
+a=9
+b=10
+print("and ope:",a>b & a<b)
+print("or ope:",a>b | a<b)
+print("left shift ope:", a<<b)
+print("right shift ope:", a>>b)
+
+print("MEMBER SHIP OPERATORS")
+a=10
+b=[1,2,3,4,5,6]
+
+print("in ope:", a in b)
+print("not in:", a not in b)
+
+print("IDENTITY OPERATOR:")
+a=10
+b=14
+print("is:",a is b)
+print("is not:",a is not b)
+
+
+
+
+
+
+
+
+
+
+
